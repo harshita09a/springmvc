682f517f4ba3e4868396e268ef19dd31ba1ad766
harshita09a <51961783+harshita09a@users.noreply.github.com>
2019-07-26 15:38:02 +0530

    Add files via upload

---
+++ jpa-demo1/src/main/java/com/cg/dao/ProductDAO.java
@@ -0,0 +1,11 @@
+package com.cg.dao;
+
+import java.util.List;
+
+import com.cg.entities.Product;
+
+public interface ProductDAO {
+	public void save(Product p);
+    public Product findById(Integer productId);
+    public List<Product> findAll();
+}
